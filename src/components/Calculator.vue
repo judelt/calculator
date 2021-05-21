dbcd892bd084bb2b8cdfc3bb4b7fcbdcb7477a90
judelt <mendez.puig@gmail.com>
2021-05-21 12:58:20 -0700

    fix bug length

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,55 +1,89 @@
 <template>
   <div class="calculator">
-    <div class="display"><div class="current">{{current || 0}}</div></div>
+    <div class="display">
+      <div
+      class="current"
+      :style="'font-size:'+fontSize+'%'">
+        {{current || '0'}}
+      </div>
+    </div>
     <button class="operator2" @click="clear">C</button>
     <button class="operator2" @click="sign">+/-</button>
     <button class="operator2" @click="percent">%</button>
     <button class="operator" @click="divide">÷</button>
-    <button @click="append(7)">7</button>
-    <button @click="append(8)">8</button>
-    <button @click="append(9)">9</button>
+    <button @click="append('7')">7</button>
+    <button @click="append('8')">8</button>
+    <button @click="append('9')">9</button>
     <button class="operator" @click="times">x</button>
-    <button @click="append(4)">4</button>
-    <button @click="append(5)">5</button>
-    <button @click="append(6)">6</button>
+    <button @click="append('4')">4</button>
+    <button @click="append('5')">5</button>
+    <button @click="append('6')">6</button>
     <button class="operator" @click="minus">-</button>
-    <button @click="append(1)">1</button>
-    <button @click="append(2)">2</button>
-    <button @click="append(3)">3</button>
+    <button @click="append('1')">1</button>
+    <button @click="append('2')">2</button>
+    <button @click="append('3')">3</button>
     <button class="operator" @click="plus">+</button>
-    <button class="zero" @click="append(0)" >0</button>
+    <button class="zero" @click="append('0')">0</button>
     <button @click="dot">.</button>
     <button class="operator" @click="equal">=</button>
   </div>
 </template>
 
 <script>
-export default {
 
+export default {
+ 
   data() {
     return {
-      previous: null,
+      currentString: '',
       current: '',
       operator: null,
       operatorClicked: false
     }
   },
+  computed: {
+     fontSize() {
+      if (this.current.length >= 10 && this.current.length < 20) {
+        return 250 - (this.current.length ** 1.57)
+      }
+      if (this.current.length >= 20) {
+        console.log('this.current.length', this.current.length)
+        return 250 - (this.current.length * 5.5)       
+      } 
+      return 250;
+    }
+  },
   methods: {
     clear() {
       this.current = '';
+      this.previous = null;
     },
     sign() {
-      this.current = this.current.charAt(0) === '-' ? this.current.slice(1) : `- ${this.current}`;
+      if (this.current !== '') {
+        this.current = this.current.charAt(0) === '-' ? this.current.slice(1) : `- ${this.current}`;
+        console.log('current', this.current)
+      } 
     },
     percent() {
       this.current = `${parseFloat(this.current) / 100}`
     },
-    append(num) {
-      if(this.operatorClicked) {
+    append(number) {
+      
+      if (this.operatorClicked) {
         this.current = '';
         this.operatorClicked = false;
       }
-      this.current = `${this.current}${num}`;
+      //max lenght 25
+      if (this.current.length >= 25) {
+        return this.current;
+      } 
+      if (this.current === '' || this.current === '0') {
+        this.current = number;
+      } else {
+        this.current = `${this.current}${number}`;
+      }
+      console.log('number', number)
+      console.log('current', this.current)
     },
     dot() {
       if(this.current.indexOf('.') === -1) this.append('.');
@@ -81,11 +115,9 @@ export default {
         parseFloat(this.current)
         )}`
       }
-      
       this.previous = null;
     },      
   }
- 
 }
 </script>
 
@@ -114,8 +146,9 @@ color: #d3e0ea;
 .current {
   align-self: center;
   justify-self:right;
-  padding-right: 8% ;
+  padding-right: 8%;
   font-size: 3em;
+  overflow: hidden;
 }
 
 .zero {
